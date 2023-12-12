14783b2fc03464c41d9347c7474d4d3584900403
angryWilly <kirill.teyxrib@yandex.ru>
2023-12-12 20:18:39 +0700

    Добавьте файлы проекта.

---
+++ Soap/Business Logic/SoapService.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceModel;
+
+namespace Soap.Business_Logic
+{
+    [ServiceContract]
+    public interface ISoapService
+    {
+        [OperationContract]
+        string ReverseString(string contextToReverse);
+
+        [OperationContract]
+        string ReverseChars(string contextToReverse);
+
+    }
+
+    public class SoapService : ISoapService
+    {
+        public string ReverseString(string contextToReverse)
+        {
+            var words = contextToReverse.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+
+        public string ReverseChars(string contextToReverse)
+        {
+            var chars= contextToReverse.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
